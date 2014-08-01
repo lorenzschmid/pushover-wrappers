540c2771c491dc57304c590bb842822dea9f7872
deep-introspection <deep@introspection.eu>
2014-08-01 12:25:40 +0200

    Python wrapper updated.

--- python2pushover.py
+++ python2pushover.py
@@ -1,14 +1,11 @@
-import httplib
- 
-# contants:
-APIkey = 'XXXXXXXXXXXXX' #Enter yours here
+import httplib, urllib
  
 def python2pushover(message='hello world'):
     conn = httplib.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",
         urllib.urlencode({
-            "token": "XXXXXXXXXXXXX",
-            "user": "XXXXXXXXXXXXX",
-            "message": msg,
+            "token": "",
+            "user": "",
+            "message": message,
     }), { "Content-type": "application/x-www-form-urlencoded" })
-    conn.getresponse()
+    return conn.getresponse()
